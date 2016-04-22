0bd8ba8fa571a901d3424faf95127dc852f7972e
paulowensmith68 <paulosmith@tiscali.co.uk>
2016-04-22 15:35:11 +0100

    Changes to Events

---
+++ HousekeepStagingService/HousekeepDatabaseClass.vb
@@ -0,0 +1,352 @@
+﻿Imports System.Xml
+Imports System.IO
+Imports System.Text
+Imports MySql.Data
+Imports MySql.Data.MySqlClient
+Public Class HousekeepDatabaseClass
+
+    ' Holds the connection string to the database used.
+    Public connectionString As String = globalConnectionString
+
+    ' Holds name of key to delete data from after inserts
+    Public strDeleteKeyType As String = ""
+
+    'Vars used for output message
+    Private insertCount As Integer = 0
+    Private updateCount As Integer = 0
+
+    'Vars used to control cursor
+    Public intCursorCount As Integer = 0
+
+    ' Store last event id so we ony log each event
+    Dim intLastEventId As Integer = 0
+
+    Public Sub IdentifyEvents()
+        '-----------------------------------------------------------------------*
+        ' Sub Routine parameters                                                *
+        ' -----------------------                                               *
+        '-----------------------------------------------------------------------*
+        Dim cno As MySqlConnection = New MySqlConnection(connectionString)
+        Dim dr As MySqlDataReader
+        Dim cmd As New MySqlCommand
+
+        ' Reset cursor counter
+        intCursorCount = 0
+
+        ' /----------------------------------------------------------------\
+        ' | MySql Select                                                   |
+        ' | Identify all past events                                       |
+        ' \----------------------------------------------------------------/
+        cmd.CommandText = "SELECT ev.`id`, ev.name, ev.`startDate` FROM Event As ev " &
+                                 "WHERE ev.`status_type` = ""finished"" AND " &
+                                 "ev.startdate < str_to_date(@todaysDate, '%Y-%m-%d %H:%i:%s') " &
+                                 "LIMIT @limit "
+
+        ' Get start and end date
+        Dim currentDateTime As DateTime = DateTime.UtcNow
+        Dim strCurrentDate As String
+
+        ' Calculate start date/time and To date/time
+        Dim centralEuropeZoneId As String = "Central Europe Standard Time"
+        Dim centralEuropeZone As TimeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById(centralEuropeZoneId)
+        strCurrentDate = TimeZoneInfo.ConvertTimeFromUtc(currentDateTime, centralEuropeZone).ToString("yyyy-MM-dd" + " 00:00:00")
+
+        cmd.Parameters.AddWithValue("todaysDate", strCurrentDate)
+        cmd.Parameters.AddWithValue("limit", My.Settings.LimitEventRows)
+
+        cmd.Connection = cno
+
+        Try
+            cno.Open()
+            dr = cmd.ExecuteReader
+
+            If dr.HasRows Then
+
+                While dr.Read()
+
+                    ' Increment counter
+                    intCursorCount = intCursorCount + 1
+
+                    Dim intEventId As Integer = dr.GetInt64(0)
+                    Dim strEventName As String = dr.GetString(1)
+                    Dim dtStartDate As DateTime = dr.GetDateTime(2).ToString("yyyy-MM-dd HH:mm:ss")
+
+                    ' Log each event
+                    If intEventId = intLastEventId Then
+                        ' Continue
+                    Else
+                        intLastEventId = intEventId
+                        gobjEvent.WriteToEventLog("HousekeepDatabaseClass:  Processing event: " + strEventName + " which was on: " + dtStartDate.ToString("yyyy-MM-dd HH:mm:ss"))
+                    End If
+
+                    ' Delete data
+                    DeleteBettingOffers(intEventId)
+                    DeleteOutcomes(intEventId)
+                    DeleteBettingOffersStaging(intEventId)
+                    DeleteOutcomesStaging(intEventId)
+                    DeleteEvent(intEventId)
+
+                End While ' End: Outer Loop
+
+            End If
+
+            ' Close the Data reader
+            dr.Close()
+
+            gobjEvent.WriteToEventLog("HousekeepDatabaseClass:  Processed Events, number of rows: " + intCursorCount.ToString)
+
+        Catch ex As Exception
+
+            gobjEvent.WriteToEventLog("HousekeepDatabaseClass:  Processing Events exception: " + ex.Message, EventLogEntryType.Error)
+
+        Finally
+            cno.Close()
+        End Try
+
+    End Sub
+
+    Private Sub DeleteBettingOffers(eventId As Integer)
+
+        Dim myConnection As New MySqlConnection(connectionString)
+        Dim cmd As New MySqlCommand
+        cmd.CommandText = "DELETE bo.* FROM bettingoffer AS bo " &
+                                "INNER JOIN outcome AS ou ON ou.`id`=bo.`outcomeFK` " &
+                                "INNER JOIN event AS ev ON ev.`id` =ou.`objectFK` " &
+                                "WHERE ou.`object`=""event"" AND ou.`objectFK`=@eventId "
+        cmd.Connection = myConnection
+        cmd.Parameters.Add(New MySqlParameter("eventId", eventId))
+
+        Try
+
+            myConnection.Open()
+            Dim rowAffected As Integer = cmd.ExecuteNonQuery()
+
+            gobjEvent.WriteToEventLog("DeleteBettingOffers : Deleted betting offers rows: " + rowAffected.ToString)
+
+        Catch ex As Exception
+
+            gobjEvent.WriteToEventLog("DeleteBettingOffers : Delete betting offers rows failed: " + ex.Message, EventLogEntryType.Error)
+
+        Finally
+
+            cmd.Dispose()
+            myConnection.Close()
+
+        End Try
+
+    End Sub
+
+    Private Sub DeleteOutcomes(eventId As Integer)
+
+        Dim myConnection As New MySqlConnection(connectionString)
+        Dim cmd As New MySqlCommand
+        cmd.CommandText = "DELETE ou.* FROM outcome AS ou " &
+                                "INNER JOIN event AS ev ON ev.`id` =ou.`objectFK` " &
+                                "WHERE ou.`object`=""event"" AND ou.`objectFK`=@eventId "
+        cmd.Connection = myConnection
+        cmd.Parameters.Add(New MySqlParameter("eventId", eventId))
+
+        Try
+
+            myConnection.Open()
+            Dim rowAffected As Integer = cmd.ExecuteNonQuery()
+
+            gobjEvent.WriteToEventLog("DeleteOutcomes : Deleted outcome rows: " + rowAffected.ToString)
+
+        Catch ex As Exception
+
+            gobjEvent.WriteToEventLog("DeleteOutcomes : Delete outcome rows failed: " + ex.Message, EventLogEntryType.Error)
+
+        Finally
+
+            cmd.Dispose()
+            myConnection.Close()
+
+        End Try
+
+    End Sub
+
+    Private Sub DeleteBettingOffersStaging(eventId As Integer)
+
+        Dim myConnection As New MySqlConnection(connectionString)
+        Dim cmd As New MySqlCommand
+        cmd.CommandText = "DELETE bxn.* FROM bookmaker_xml_nodes AS bxn " &
+                                "INNER JOIN outcome AS ou ON ou.`id`=bxn.`outcome_Id` " &
+                                "INNER JOIN event AS ev ON ev.`id` =ou.`objectFK` " &
+                                "WHERE ou.`object`=""event"" AND ou.`objectFK`=@eventId "
+        cmd.Connection = myConnection
+        cmd.Parameters.Add(New MySqlParameter("eventId", eventId))
+
+        Try
+
+            myConnection.Open()
+            Dim rowAffected As Integer = cmd.ExecuteNonQuery()
+
+            gobjEvent.WriteToEventLog("DeleteBettingOffers : Deleted betting offers from bookmaker_xml_nodes rows: " + rowAffected.ToString)
+
+        Catch ex As Exception
+
+            gobjEvent.WriteToEventLog("DeleteBettingOffers : Delete betting offers from bookmaker_xml_nodes rows failed: " + ex.Message, EventLogEntryType.Error)
+
+        Finally
+
+            cmd.Dispose()
+            myConnection.Close()
+
+        End Try
+
+    End Sub
+
+    Private Sub DeleteOutcomesStaging(eventId As Integer)
+
+        'Delete id from saved_xml (log table)
+        Dim myConnection As New MySqlConnection(connectionString)
+        Dim cmd As New MySqlCommand
+        cmd.CommandText = "DELETE bxn.* FROM bookmaker_xml_nodes AS bxn " &
+                          "WHERE  bxn.`event_id`=@eventId "
+        cmd.Connection = myConnection
+        cmd.Parameters.Add(New MySqlParameter("eventId", eventId))
+
+        Try
+
+            myConnection.Open()
+            Dim rowAffected As Integer = cmd.ExecuteNonQuery()
+
+            gobjEvent.WriteToEventLog("DeleteOutcomesStaging : Deleted outcomes from bookmaker_xml_nodes rows: " + rowAffected.ToString)
+
+        Catch ex As Exception
+
+            gobjEvent.WriteToEventLog("DeleteOutcomesStaging : Delete outcomes from bookmaker_xml_nodes rows failed: " + ex.Message, EventLogEntryType.Error)
+
+        Finally
+
+            cmd.Dispose()
+            myConnection.Close()
+
+        End Try
+
+    End Sub
+
+    Private Sub DeleteEvent(eventId As Integer)
+
+        Dim myConnection As New MySqlConnection(connectionString)
+        Dim myCommand As New MySqlCommand("delete from event where `id`=@id")
+        myCommand.CommandType = CommandType.Text
+        myCommand.Connection = myConnection
+        myCommand.Parameters.Add(New MySqlParameter("id", eventId))
+
+        Try
+
+            myConnection.Open()
+            Dim rowAffected As Integer = myCommand.ExecuteNonQuery()
+
+            gobjEvent.WriteToEventLog("DeleteEvent : Deleted event rows: " + rowAffected.ToString)
+
+        Catch ex As Exception
+
+            gobjEvent.WriteToEventLog("DeleteEvent : Delete event rows failed: " + ex.Message, EventLogEntryType.Error)
+
+        Finally
+
+            myCommand.Dispose()
+            myConnection.Close()
+
+        End Try
+
+    End Sub
+
+    Public Sub DeleteStaleBettingOffersStaging()
+
+        Dim myConnection As New MySqlConnection(connectionString)
+        Dim cmd As New MySqlCommand
+        cmd.CommandTimeout = 360
+
+        cmd.CommandText = "DELETE FROM bookmaker_xml_nodes " &
+                          "WHERE nodeName = ""bettingoffer"" AND " &
+                          "ut < str_to_date(@dateTime, '%Y-%m-%d %H:%i:%s') " &
+                          "LIMIT @limit "
+
+        ' Get start and end date
+        Dim currentDateTime As DateTime = DateTime.UtcNow
+        Dim strParamDate As String
+        Dim twoHoursAgoDateTime As DateTime
+
+        ' Calculate start date/time and To date/time
+        Dim centralEuropeZoneId As String = "UTC"
+        Dim centralEuropeZone As TimeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById(centralEuropeZoneId)
+        Dim intMinutes = My.Settings.HousekeepStaleBetsAfterMins * -1
+        twoHoursAgoDateTime = currentDateTime.AddMinutes(intMinutes)
+        strParamDate = TimeZoneInfo.ConvertTimeFromUtc(twoHoursAgoDateTime, centralEuropeZone).ToString("yyyy-MM-dd HH:mm:ss")
+
+        cmd.Parameters.AddWithValue("dateTime", strParamDate)
+        cmd.Parameters.AddWithValue("limit", My.Settings.LimitStaleRows)
+        cmd.Connection = myConnection
+
+        Try
+
+            myConnection.Open()
+            Dim rowAffected As Integer = cmd.ExecuteNonQuery()
+
+            gobjEvent.WriteToEventLog("DeleteStaleBettingOffersStaging : Deleted stale BettingOffers Staging rows: " + rowAffected.ToString)
+
+        Catch ex As Exception
+
+            gobjEvent.WriteToEventLog("DeleteStaleBettingOffersStaging : Delete stale BettingOffers Staging rows failed: " + ex.Message, EventLogEntryType.Error)
+
+        Finally
+
+            cmd.Dispose()
+            myConnection.Close()
+
+        End Try
+
+    End Sub
+
+    Public Sub DeleteStaleOutcomesStaging()
+
+        Dim myConnection As New MySqlConnection(connectionString)
+        Dim cmd As New MySqlCommand
+        cmd.CommandTimeout = 360
+
+        cmd.CommandText = "DELETE FROM bookmaker_xml_nodes " &
+                          "WHERE nodeName = ""outcome"" AND " &
+                          "ut < str_to_date(@dateTime, '%Y-%m-%d %H:%i:%s') " &
+                          "LIMIT @limit "
+
+        ' Get start and end date
+        Dim currentDateTime As DateTime = DateTime.UtcNow
+        Dim strParamDate As String
+        Dim twoHoursAgoDateTime As DateTime
+
+        ' Calculate start date/time and To date/time
+        Dim centralEuropeZoneId As String = "UTC"
+        Dim centralEuropeZone As TimeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById(centralEuropeZoneId)
+        Dim intMinutes = My.Settings.HousekeepStaleBetsAfterMins * -1
+        twoHoursAgoDateTime = currentDateTime.AddMinutes(intMinutes)
+        strParamDate = TimeZoneInfo.ConvertTimeFromUtc(twoHoursAgoDateTime, centralEuropeZone).ToString("yyyy-MM-dd HH:mm:ss")
+
+        cmd.Parameters.AddWithValue("dateTime", strParamDate)
+        cmd.Parameters.AddWithValue("limit", My.Settings.LimitStaleRows)
+        cmd.Connection = myConnection
+
+        Try
+
+            myConnection.Open()
+            Dim rowAffected As Integer = cmd.ExecuteNonQuery()
+
+            gobjEvent.WriteToEventLog("DeleteStaleOutcomesStaging : Deleted stale Outcomes Staging rows: " + rowAffected.ToString)
+
+        Catch ex As Exception
+
+            gobjEvent.WriteToEventLog("DeleteStaleOutcomesStaging : Delete stale Outcomes Staging rows failed: " + ex.Message, EventLogEntryType.Error)
+
+        Finally
+
+            cmd.Dispose()
+            myConnection.Close()
+
+        End Try
+
+    End Sub
+
+End Class
--- HousekeepStagingService/My Project/Settings.Designer.vb
+++ HousekeepStagingService/My Project/Settings.Designer.vb
@@ -13,57 +13,176 @@ Option Explicit On
 
 
 Namespace My
-
-    <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(), _
-     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0"), _
-     Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)> _
+    
+    <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
+     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0"),  _
+     Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
     Partial Friend NotInheritable Class MySettings
         Inherits Global.System.Configuration.ApplicationSettingsBase
-
-        Private Shared defaultInstance As MySettings = CType(Global.System.Configuration.ApplicationSettingsBase.Synchronized(New MySettings), MySettings)
-
+        
+        Private Shared defaultInstance As MySettings = CType(Global.System.Configuration.ApplicationSettingsBase.Synchronized(New MySettings()),MySettings)
+        
 #Region "My.Settings Auto-Save Functionality"
 #If _MyType = "WindowsForms" Then
-        Private Shared addedHandler As Boolean
+    Private Shared addedHandler As Boolean
 
-        Private Shared addedHandlerLockObject As New Object
+    Private Shared addedHandlerLockObject As New Object
 
-        <Global.System.Diagnostics.DebuggerNonUserCodeAttribute(), Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)> _
-        Private Shared Sub AutoSaveSettings(ByVal sender As Global.System.Object, ByVal e As Global.System.EventArgs)
-            If My.Application.SaveMySettingsOnExit Then
-                My.Settings.Save()
-            End If
-        End Sub
+    <Global.System.Diagnostics.DebuggerNonUserCodeAttribute(), Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)> _
+    Private Shared Sub AutoSaveSettings(ByVal sender As Global.System.Object, ByVal e As Global.System.EventArgs)
+        If My.Application.SaveMySettingsOnExit Then
+            My.Settings.Save()
+        End If
+    End Sub
 #End If
 #End Region
-
+        
         Public Shared ReadOnly Property [Default]() As MySettings
             Get
-
+                
 #If _MyType = "WindowsForms" Then
-                   If Not addedHandler Then
-                        SyncLock addedHandlerLockObject
-                            If Not addedHandler Then
-                                AddHandler My.Application.Shutdown, AddressOf AutoSaveSettings
-                                addedHandler = True
-                            End If
-                        End SyncLock
-                    End If
+               If Not addedHandler Then
+                    SyncLock addedHandlerLockObject
+                        If Not addedHandler Then
+                            AddHandler My.Application.Shutdown, AddressOf AutoSaveSettings
+                            addedHandler = True
+                        End If
+                    End SyncLock
+                End If
 #End If
                 Return defaultInstance
             End Get
+        End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("60")>  _
+        Public Property ProcessCycleEverySecs() As Integer
+            Get
+                Return CType(Me("ProcessCycleEverySecs"),Integer)
+            End Get
+            Set
+                Me("ProcessCycleEverySecs") = value
+            End Set
+        End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("C:\#myPrograms\HousekeepStagingService\logs\")>  _
+        Public Property ProcessLogPath() As String
+            Get
+                Return CType(Me("ProcessLogPath"),String)
+            End Get
+            Set
+                Me("ProcessLogPath") = value
+            End Set
+        End Property
+        
+        <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.ConnectionString),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("Database=OddsMatching;Data Source=eu-cdbr-azure-north-e.cloudapp.net;User Id=b083"& _ 
+            "cb50265fec;Password=263fb5f7;persistsecurityinfo=True")>  _
+        Public ReadOnly Property ConnectionString() As String
+            Get
+                Return CType(Me("ConnectionString"),String)
+            End Get
+        End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("True")>  _
+        Public Property LoadOutcomesAndOffers() As Boolean
+            Get
+                Return CType(Me("LoadOutcomesAndOffers"),Boolean)
+            End Get
+            Set
+                Me("LoadOutcomesAndOffers") = value
+            End Set
+        End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("50")>  _
+        Public Property LimitEventRows() As Integer
+            Get
+                Return CType(Me("LimitEventRows"),Integer)
+            End Get
+            Set
+                Me("LimitEventRows") = value
+            End Set
+        End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("1000")>  _
+        Public Property LimitStaleRows() As Integer
+            Get
+                Return CType(Me("LimitStaleRows"),Integer)
+            End Get
+            Set
+                Me("LimitStaleRows") = value
+            End Set
+        End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("True")>  _
+        Public Property HousekeepPastEvents() As Boolean
+            Get
+                Return CType(Me("HousekeepPastEvents"),Boolean)
+            End Get
+            Set
+                Me("HousekeepPastEvents") = value
+            End Set
+        End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("True")>  _
+        Public Property HousekeepStagingTables() As String
+            Get
+                Return CType(Me("HousekeepStagingTables"),String)
+            End Get
+            Set
+                Me("HousekeepStagingTables") = value
+            End Set
+        End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("15")>  _
+        Public Property HousekeepStaleBetsAfterMins() As Integer
+            Get
+                Return CType(Me("HousekeepStaleBetsAfterMins"),Integer)
+            End Get
+            Set
+                Me("HousekeepStaleBetsAfterMins") = value
+            End Set
+        End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("A")>  _
+        Public Property LogStreamId() As String
+            Get
+                Return CType(Me("LogStreamId"),String)
+            End Get
+            Set
+                Me("LogStreamId") = value
+            End Set
         End Property
     End Class
 End Namespace
 
 Namespace My
-
-    <Global.Microsoft.VisualBasic.HideModuleNameAttribute(), _
-     Global.System.Diagnostics.DebuggerNonUserCodeAttribute(), _
-     Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute()> _
+    
+    <Global.Microsoft.VisualBasic.HideModuleNameAttribute(),  _
+     Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+     Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute()>  _
     Friend Module MySettingsProperty
-
-        <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")> _
+        
+        <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
         Friend ReadOnly Property Settings() As Global.HousekeepStagingService.My.MySettings
             Get
                 Return Global.HousekeepStagingService.My.MySettings.Default
